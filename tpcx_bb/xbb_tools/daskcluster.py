649bf7e8e0260354c7ebbb03e6285bbce83a2ccb
Kevin German <kgerman@nvidia.com>
2020-09-08 11:27:45 -0700

    daskcluster args and env
    Using _DASK prefix for env, but conf variables do not use the prefix
    mem limits in MB
    handle cluster mode variants as default
    handle with_blazing case

--- tpcx_bb/xbb_tools/daskcluster.py
+++ tpcx_bb/xbb_tools/daskcluster.py
@@ -22,9 +22,11 @@ import subprocess
 import logging, os, sys, math, time
 log=logging.getLogger()
 
+
 """
    Thin wrapper around a thin wrapper to use args from xbb_tools.config to call
    Start stop dask cluster for automated benchmarks.
+   * config uses environment prefix 'DASK_'
 """
 
 def cli(commandline=None):
@@ -32,36 +34,24 @@ def cli(commandline=None):
 
     parser.add_argument(
         'commands', nargs='*',
-        help='one or more of.. start_workers, stop_workers, start_scheduler, stop_scheduler.  More details: help-commands'
+        help='one or more of.. start_workers, stop_workers, start_scheduler, stop_scheduler.'
     )
     parser.usage="Start and stop, and get data from dask cluster described in config file."
     args = vars(parser.parse_args( commandline ))
     conf=config.get_config( args, fname=args.get('configfile'), envprefix='DASK_')
 
-    env={'CUDA_VISIBLE_DEVICES':conf.get('CUDA_VISIBLE_DEVICES',os.getenv(
-            'CUDA_VISIBLE_DEVICES',visible_devices()))}
+    env={'CUDA_VISIBLE_DEVICES':conf.get(
+            'CUDA_VISIBLE_DEVICES',visible_devices()),
+         'DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT':conf.get(
+            'distributed__comm__timeouts__connect', "100s"),
+         'DASK_DISTRIBUTED__COMM__TIMEOUTS__TCP':conf.get(
+            'distrubuted__comm__timeouts__tcp', "600s"),
+         'DASK_DISTRIBUTED__COMM__RETRY__DELAY__MIN':conf.get(
+            'distributed__comm__retry__delay__min', "1s"),
+         'DASK_DISTRIBUTED__COMM__RETRY__DELAY__MAX':conf.get(
+            'distributed__comm__retry__delay__max', "60s")}
 
-    if conf.cluster_mode == "TCP":
-        env.update({'DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT':conf.get(
-                        'distributed__comm__timeouts__connect', "100s"),
-                    'DASK_DISTRIBUTED__COMM__TIMEOUTS__TCP':conf.get(
-                        'distrubuted__comm__timeouts__tcp', "600s"),
-                    'DASK_DISTRIBUTED__COMM__RETRY__DELAY__MIN':conf.get(
-                        'distributed__comm__retry__delay__min', "1s"),
-                    'DASK_DISTRIBUTED__COMM__RETRY__DELAY__MAX':conf.get(
-                        'distributed__comm__retry__delay__max', "60s")
-                    })
-    elif conf.cluster_mode=='NVLINK':
-        env.update({'tpcxbb_benchmark_sweep_run':bool(conf.get('tpcxbb_benchmark_sweep_run',True)),
-                    'DASK_RMM__POOL_SIZE':conf.get('rmm__pool_size','1GB'),
-                    'DASK_UCX__CUDA_COPY':conf.get('ucx__cuda_copy',True),
-                    'DASK_UCX__TCP':conf.get('ucx__tcp',True),
-                    'DASK_UCX__NVLINK':conf.get('ucx__nvlink',True),
-                    'DASK_UCX__INFINIBAND':conf.get('ucx_infiniband',False),
-                    'DASK_UCX__RDMACM':conf.get('ucx__rdmacm',False),
-                    })
-
-    for cmd in conf.commands:
+    for i, cmd in enumerate(conf.commands):
         cmdf = cmd.upper().strip().replace('-','_')
         if cmdf == 'START_WORKERS':
             start_workers( conf, env )
@@ -78,43 +68,58 @@ def cli(commandline=None):
         time.sleep(2)
 
 def start_scheduler( conf, env ):
-    clusterkeys=( 'dashboard_address', 'diagnostics_port', 'dask_host',
-                  'dask_interface', 'dask_port', 'dask_preload',
-                  'dask_protocol', 'dask_scheduler-file' )
+    clusterkeys=( 'dashboard_address', 'diagnostics_port', 'host',
+                  'interface', 'port', 'preload',
+                  'protocol', 'scheduler-file' )
 
-    args=[str(a) for k in filter( lambda x: x in clusterkeys, conf.keys()) for a in [k.replace('dask_','--'),conf.get(k)]]
+    if conf.cluster_mode=='NVLINK':
+        env.update({'tpcxbb_benchmark_sweep_run':conf.get('tpcxbb_benchmark_sweep_run',True),
+                    'DASK_RMM__POOL_SIZE':conf.get('rmm__pool_size','1GB'),
+                    'DASK_UCX__CUDA_COPY':conf.get('ucx__cuda_copy',True),
+                    'DASK_UCX__TCP':conf.get('ucx__tcp',True),
+                    'DASK_UCX__NVLINK':conf.get('ucx__nvlink',True),
+                    'DASK_UCX__INFINIBAND':conf.get('ucx_infiniband',False),
+                    'DASK_UCX__RDMACM':conf.get('ucx__rdmacm',False),
+                    })
+
+    args=[str(a) for k in filter( lambda x: x in clusterkeys, conf.keys()) for a in [f"--{k}",conf.get(k)]]
     return subprocess.Popen([sys.executable, '-m', 'distributed.cli.dask_scheduler'] + args,
                             stdout=open( os.path.join( conf.get('logdir', os.getcwd()), 'scheduler_{}.stdout.log'.format(os.getpid())), 'w'),
                             stderr=open( os.path.join( conf.get('logdir', os.getcwd()), 'scheduler_{}.stderr.log'.format(os.getpid())), 'w'),
-                            close_fds=True, env,
-                            restore_signals=False, start_new_session=True)
+                            close_fds=True, env=env, restore_signals=False, start_new_session=True)
 
 def start_workers( conf, env ):
-    nworkers=int(conf.dask_n_workers)
-    gpu_max_mem=float(device_memory_limit())
-    device_mem_limit="{}MB".format(int(float(conf.get('dask_device-memory-limit',gpu_max_mem))/(1024**2)*.8))
-    sys_max_mem="{}MB".format( int(memory_limit()/(int(nworkers)*1024**2)))
-    env.update({'DEVICE_MEMORY_LIMIT':device_mem_limit,
-                'POOL_SIZE':'{}MB'.format( int(gpu_max_mem/(1024**2))),
+    nworkers=int(conf.n_workers)
+    gpu_max_mem_mb=float(device_memory_limit())/(1024**2)
+
+    device_mem_limit_mb=int(conf.get('device-memory-limit',0.8)*gpu_max_mem_mb)
+    sys_max_mem_mb=int(memory_limit()/(int(nworkers)*1024**2))
+
+    env.update({'DEVICE_MEMORY_LIMIT':f"{device_mem_limit_mb}MB",
+                'POOL_SIZE':F'{gpu_max_mem_mb}MB',
                 'LOGDIR':conf.get('log_dir', './'),
-                'WORKER_DIR':conf.get('dask_worker_dir','./'),
-                'MAX_SYSTEM_MEMORY':sys_max_mem})
+                'WORKER_DIR':conf.get('worker_dir','./'),
+                'MAX_SYSTEM_MEMORY':f"{sys_max_mem_mb}MB"})
 
     args=[sys.executable, '-m', 'dask_cuda.cli.dask_cuda_worker',
-          '--device-memory-limit', device_mem_limit, '--no-reconnect',
-          '--{}-tcp-over-ucx'.format(conf.get('dask_tcp-over-ucx','disable')), 
-          '--memory-limit', conf.get('dask_memory-limit',sys_max_mem), 
-          '--rmm-pool-size', conf.get('dask_rmm-pool-size',device_mem_limit),
-          '--{}-infiniband'.format( conf.get('dask_infiniband','disable')),
-          '--{}-nvlink'.format( conf.get( 'dask_nvlink', 'disable')),
-          '--{}-rdmacm'.format( conf.get( 'dask_rdmacm', 'disable'))]
+          '--device-memory-limit', f"{device_mem_limit_mb}MB", '--no-reconnect',
+          '--memory-limit', conf.get('memory-limit',f"{sys_max_mem_mb}MB"),
+          '--{}-tcp-over-ucx'.format(conf.get('tcp-over-ucx','enable' if conf.cluster_mode.lower() == "NVLINK" else "disable")),
+          '--{}-infiniband'.format( conf.get('infiniband','disable' if conf.cluster_mode.lower() == "NVLINK" else "enable")),
+          '--{}-nvlink'.format( conf.get( 'nvlink', 'enable' if conf.cluster_mode.lower() == "NVLINK" else "disable")),
+          '--{}-rdmacm'.format( conf.get( 'rdmacm', 'disable'))]
 
-    clusterkeys=( 'dask_diagnostics_port', 'dask_host', 'dask_local-directory',
-                  'dask_interface', 'dask_port', 'dask_preload',
-                  'dask_scheduler-file', 'dask_net-devices', 'dask_nthreads',
-                  'dask_tls-ca-file', 'dask_tls-cert', 'dask_tls-key' )
+    if conf.with_blazing:
+        ##with blazing: RMM_POOL_SIZE is reduced from total GPU mem by half the amount that DEVICE_MEMORY_LIMIT
+        args+=['--rmm-pool-size',conf.get('rmm-pool-size', "{}MB".format(
+                                          int(gpu_mem_max_mb*(float(device_mem_limit_mb)/(2*gpu_max_mem_mb)))))]
 
-    args+=[str(a) for k in filter( lambda x: x in clusterkeys, conf.keys()) for a in [k.replace('dask_','--'),conf.get(k)]]
+    clusterkeys=( 'diagnostics_port', 'host', 'local-directory',
+                  'interface', 'port', 'preload',
+                  'scheduler-file', 'net-devices', 'nthreads',
+                  'tls-ca-file', 'tls-cert', 'tls-key' )
+
+    args+=[str(a) for k in filter( lambda x: x in clusterkeys, conf.keys()) for a in [f"--{k}",conf.get(k)]]
     #print ("EXECUTE:  {}".format( ' '.join( map(str,args))))
 
     visible_gpus=visible_devices()
